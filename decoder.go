984866275b4a06b0659de979228ade47c28f2d23
lwnmengjing <lwnmengjing@qq.com>
2022-08-01 15:00:58 +0800

    :tada: enable

---
+++ decoder.go
@@ -0,0 +1,50 @@
+package cadenceDecoder
+
+import "github.com/onflow/cadence"
+
+// Decode decode cadence to json
+func Decode(value cadence.Value) (any, error) {
+	var err error
+	switch value := value.(type) {
+	case cadence.Array:
+		data := make([]any, len(value.Values))
+		for i := range value.Values {
+			data[i], err = Decode(value.Values[i])
+			if err != nil {
+				return data, err
+			}
+		}
+		return data, nil
+	case cadence.Optional:
+		return Decode(value.Value)
+	case cadence.Struct:
+		data := make(map[string]any)
+		for i := range value.StructType.Fields {
+			v, err := Decode(value.Fields[i])
+			if err != nil {
+				return data, err
+			}
+			if v != nil {
+				data[value.StructType.Fields[i].Identifier] = v
+			}
+		}
+		return data, nil
+	case cadence.Dictionary:
+		data := make(map[string]any)
+		for i := range value.Pairs {
+			v, err := Decode(value.Pairs[i].Value)
+			if err != nil {
+				return data, err
+			}
+			if v != nil {
+				data[value.Pairs[i].Key.String()] = v
+			}
+		}
+		return data, nil
+	default:
+		if value == nil {
+			return nil, nil
+		}
+		return value.ToGoValue(), nil
+	}
+}
---
+++ go.mod
@@ -0,0 +1,25 @@
+module github.com/mss-boot-io/cadenceDecoder
+
+go 1.18
+
+require github.com/onflow/cadence v0.24.7
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fxamacker/cbor/v2 v2.4.1-0.20220515183430-ad2eae63303f // indirect
+	github.com/fxamacker/circlehash v0.3.0 // indirect
+	github.com/go-test/deep v1.0.5 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.12 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/logrusorgru/aurora v0.0.0-20200102142835-e9ef32dff381 // indirect
+	github.com/onflow/atree v0.3.1-0.20220531231935-525fbc26f40a // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rivo/uniseg v0.2.1-0.20211004051800-57c86be7915a // indirect
+	github.com/stretchr/testify v1.7.1 // indirect
+	github.com/turbolent/prettier v0.0.0-20210613180524-3a3f5a5b49ba // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	github.com/zeebo/blake3 v0.2.3 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+)
